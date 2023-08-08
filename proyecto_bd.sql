abbcf8cfb6076914a17be8330f9a2370f45e78eb
andycg02 <127338006+andycg02@users.noreply.github.com>
2023-08-08 14:45:59 -0600

    Add files via upload

---
+++ proyecto_bd.sql
@@ -0,0 +1,81 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 08-08-2023 a las 20:44:53
+-- Versión del servidor: 8.0.31
+-- Versión de PHP: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `proyecto_bd`
+--
+
+USE proyecto_bd;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `contenido`
+--
+
+DROP TABLE IF EXISTS `contenido`;
+CREATE TABLE IF NOT EXISTS `contenido` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `contenido`
+--
+
+INSERT INTO `contenido` (`id`, `descripcion`) VALUES
+(5, 'Contenido 1');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cuestionario`
+--
+
+DROP TABLE IF EXISTS `cuestionario`;
+CREATE TABLE IF NOT EXISTS `cuestionario` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `pregunta` varchar(40) DEFAULT NULL,
+  `contenidoC` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `foreignkeyCuestionarioContenido` (`contenidoC`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `cuestionario`
+--
+
+INSERT INTO `cuestionario` (`id`, `pregunta`, `contenidoC`) VALUES
+(5, 'Pregunta 1', 5);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `cuestionario`
+--
+ALTER TABLE `cuestionario`
+  ADD CONSTRAINT `foreignkeyCuestionarioContenido` FOREIGN KEY (`contenidoC`) REFERENCES `contenido` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
