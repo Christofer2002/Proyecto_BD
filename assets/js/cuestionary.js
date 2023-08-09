6b87a3bf227ff48d0a8e6bcb415414fa88091e0c
Erxiel <votexgamer281@gmail.com>
2023-08-08 18:59:49 -0600

    Procesos agregados

--- assets/js/cuestionary.js
+++ assets/js/cuestionary.js
@@ -1,7 +1,23 @@
-/* Este listener lo que hace es tomar cada uno de los checkbox del arbol dom y les asigna un comportamiento el cual consiste en que si se selecciona uno,
-se busca si algún otro checkbox fue seleccionado, si es así, le quita el check*/
+document.addEventListener('DOMContentLoaded', () => {
+    uncheck();
+});
 
-document.addEventListener('DOMContentLoaded', function() {
+
+
+// CREO QUE HAY QUE CAMBIAR LA ESTRUCTURA
+const evaluateButton = document.getElementById('evaluateButton');
+
+evaluateButton.addEventListener('click', () => {
+    changeColorSemaphore(evaluate());
+});
+
+
+
+/**
+ * The function uncheck() is used to uncheck all checkboxes in a group when one checkbox in the group
+ * is checked.
+ */
+function uncheck(){
     const rows = document.querySelectorAll('tr'); // Obtener todas las filas
     rows.forEach(row => {
         const checkboxGroups = row.querySelectorAll('.checkbox-group');
@@ -17,4 +33,47 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         });
     });
-});
+}
+
+
+
+
+
+// ESTO NO ESTÁ TERMINADO
+function evaluate (){
+    const AffirmativechecklistItems = document.querySelectorAll('td.column-S input[type="checkbox"]');
+    const totalCheckboxes = document.querySelectorAll('tr');
+    
+    return (checkedCount / totalCheckboxes) * 100;
+}
+
+
+
+
+/**
+ * The function changes the color of a semaphore based on a given percentage.
+ * @param percentage - The `percentage` parameter represents the percentage value that determines the
+ * color of the semaphore lights.
+ */
+
+function changeColorSemaphore(percentage){
+    const luzRoja = document.querySelector('.luz luz-roja');
+    const luzAmarilla = document.querySelector('.luz luz-amarilla');
+    const luzVerde = document.querySelector('.luz luz-verde');
+
+    if (percentage < 50) {
+        luzRoja.style.backgroundColor = 'red';
+        luzAmarilla.style.backgroundColor = 'gray';
+        luzVerde.style.backgroundColor = 'gray';
+    } else if (percentage >= 50 && percentage <= 80) {
+        luzRoja.style.backgroundColor = 'gray';
+        luzAmarilla.style.backgroundColor = 'yellow';
+        luzVerde.style.backgroundColor = 'gray';
+    } else {
+        luzRoja.style.backgroundColor = 'gray';
+        luzAmarilla.style.backgroundColor = 'gray';
+        luzVerde.style.backgroundColor = 'green';
+    }
+}
+
+
